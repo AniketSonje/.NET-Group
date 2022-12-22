2b13ec404b00cd44eddbd1370cf13159905fe958
Kunal Buwa <121018324+Kunal-Buwa@users.noreply.github.com>
2022-12-22 18:58:48 +0530

    Delete kunal directory

--- kunal/server.js
+++
@@ -1,8 +0,0 @@
-var express =require ("express");
-var app =express();
-app.get ("/",function(req,resp)
-{
-    resp.send("<h1>first drop in repository</h1>");
-})
-app.listen(3000);
-console.log("listening to port 3000")
